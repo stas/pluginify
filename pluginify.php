bf078f3d6e99b5b6f50dca49322862d16ef84445
Mert Yazıcıoğlu <merty92@gmail.com>
2011-09-07 01:32:04 +0300

    Added a check to terminate the process if the given plugin name is not available on WordPress Plugin Directory.

--- pluginify.php
+++ pluginify.php
@@ -117,6 +117,12 @@
          * @return Bool, true on success or false on failure
          */
         function process( $plugin ) {
+            
+            $headers = get_headers( "http://plugins.svn.wordpress.org/{$plugin}/" );
+
+            if ( $headers['0'] !== 'HTTP/1.1 404 Not Found' )
+                die( 'This name is not available!' );
+            
             $whoami = self::whoami();
             
             if ( !self::make_dirs( $plugin ) )
